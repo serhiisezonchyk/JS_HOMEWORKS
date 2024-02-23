ea5aca630daefdc90db0d2d2c8f8be3de630d383
serhiisezonchyk <sergey040901@gmail.com>
2024-02-23 11:43:40 +0200

    hw6/done

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const firstMessage = prompt('Enter your first message.');
+const secondMessage = prompt('Enter your second message.');
+const thirdMessage = prompt('Enter your third message.');
+
+console.log(firstMessage + ' ' + secondMessage + ' ' + thirdMessage);
+
+const userNum = 23546;
+console.log(userNum.toString().split('').join(' '));
