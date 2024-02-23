9888c8ce5735abce4bf02cd8570bc99fa89d4080
serhiisezonchyk <sergey040901@gmail.com>
2024-02-23 11:57:59 +0200

    hw7/done

---
+++ js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//TASK1
+let name;
+let admin;
+
+name = 'John';
+admin = name;
+
+alert(admin);
+
+//TASK2
+const ourPlanet = 'Earth';
+const userName = 'Serhii Sezonchyk';
+
+//TASK3
+const str = '10' + 10;
+console.log(typeof str, str);
