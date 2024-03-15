b2e09eaa8d57d8d7b134c18ec7123d79ee8579e2
serhiisezonchyk <sergey040901@gmail.com>
2024-03-15 09:48:35 +0200

    HW30/DONR

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+const onlyPositive = (arr) => {
+  if (!Array.isArray(arr))
+    return `Аргумент повинен бути масивом. Ви передали ${typeof arr}`;
+  if (arr.length === 0) return `Передайте не порожній масив`;
+
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const curr = arr.at(i);
+    if (Math.sign(curr) !== -1) newArr.push(curr);
+  }
+  return newArr.length ? newArr : null;
+};
+
+const arr = [1, 2, 3, -1, -2, -3];
+const positiveArr = onlyPositive(arr);
+console.log(positiveArr);
