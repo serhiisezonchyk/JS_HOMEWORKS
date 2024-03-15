44927ca49efc516667a25fcdb19573c59e29867d
serhiisezonchyk <sergey040901@gmail.com>
2024-03-15 09:29:12 +0200

    fix: format

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const padString = (str, length, symbol = '*', left = false) => {
   if (typeof str !== 'string' || str?.length === 0) return 'Не валідний рядок.';
   if (typeof length !== 'number' || length <= 0) return 'Не валідна довжина.';
-  if (typeof symbol !=='string') return 'Не валідний символ.';
+  if (typeof symbol !== 'string') return 'Не валідний символ.';
   if (typeof left !== 'boolean') return 'Не валідний флаг.';
 
   if (length <= str.length) {
@@ -12,7 +12,7 @@ const padString = (str, length, symbol = '*', left = false) => {
   const padding = symbol.repeat(padLength);
   return left ? padding + str : str + padding;
 };
-console.log(padString('hello', 8,true));
+console.log(padString('hello', 8, true));
 console.log(padString('hello', 6, '*', false));
 console.log(padString('hello', 6, '*', true));
 console.log(padString('hello', 2));
