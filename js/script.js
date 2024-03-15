eea650462bc248f1287f863b9ed1f15b4e57f5b5
serhiisezonchyk <sergey040901@gmail.com>
2024-03-15 08:38:35 +0200

    HW28/DONE

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+const isValidNum = (num) => {
+  if (num === null) return false;
+  if (num.length === 0 || isNaN(+num)) return false;
+  return true;
+};
+const pow = (num, exp = 1) => {
+  if (isValidNum(num) && isValidNum(exp)) return num ** exp;
+  return false;
+};
+
+const num = prompt('Введіть число');
+const exp = prompt('Введіть степінь');
+
+const result = pow(num, exp);
+result === false ? alert('Помилка') : alert(`${num}**${exp} = ${result}`);
