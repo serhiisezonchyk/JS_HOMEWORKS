a79ba9bdcd51f36110345af0ceaa38c9b169bd87
serhiisezonchyk <sergey040901@gmail.com>
2024-03-15 09:28:23 +0200

    HW29/DONE

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+const padString = (str, length, symbol = '*', left = false) => {
+  if (typeof str !== 'string' || str?.length === 0) return 'Не валідний рядок.';
+  if (typeof length !== 'number' || length <= 0) return 'Не валідна довжина.';
+  if (typeof symbol !=='string') return 'Не валідний символ.';
+  if (typeof left !== 'boolean') return 'Не валідний флаг.';
+
+  if (length <= str.length) {
+    return str.substring(0, length);
+  }
+  const padLength = length - str.length;
+  const padding = symbol.repeat(padLength);
+  return left ? padding + str : str + padding;
+};
+console.log(padString('hello', 8,true));
+console.log(padString('hello', 6, '*', false));
+console.log(padString('hello', 6, '*', true));
+console.log(padString('hello', 2));
