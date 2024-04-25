40d11125ba750b89a5291e1586144b6bea241162
serhiisezonchyk <sergey040901@gmail.com>
2024-04-25 23:09:12 +0300

    HW60/Done

--- src/js/solution.js
+++ src/js/solution.js
@@ -1,4 +1,22 @@
 (function () {
-  const ulNode = document.getElementById('ulId');
+  const ulId = document.getElementById('ulId');
+  const ulIdAttributes = ulId.attributes;
+  console.log(ulIdAttributes);
 
+  const ulIdAttribute = {
+    names: [],
+    values: [],
+  };
+
+  for (const el of ulIdAttributes) {
+    ulIdAttribute.names.push(el.name);
+    ulIdAttribute.values.push(el.value);
+  }
+  console.log(ulIdAttribute.names);
+  console.log(ulIdAttribute.values);
+
+  ulId.lastElementChild.textContent = 'Hi, my name is Serhii';
+  ulId.firstElementChild.setAttribute('data-my-name', 'Serhii');
+
+  ulId.removeAttribute('data-dog-tail');
 })();
