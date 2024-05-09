60e1de7afd3960605e8c65da870184b8d3603b2d
serhiisezonchyk <sergey040901@gmail.com>
2024-05-09 09:58:46 +0300

    fix: refactor/fix creating ul in li

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,18 @@
 (function () {
-
   const generateList = (arr) => {
     const list = document.createElement('ul');
     list.classList.add('list');
     for (const el of arr) {
-      if (Array.isArray(el)) {
-        list.append(generateList(el));
-      } else {
-        const listEl = document.createElement('li');
-        listEl.classList.add('list__el');
-        listEl.innerHTML = el;
-        list.appendChild(listEl);
-      }
+      const listEl = document.createElement('li');
+      listEl.classList.add('list__el');
+      Array.isArray(el) ? listEl.appendChild(generateList(el)) : (listEl.innerHTML = el);
+      list.appendChild(listEl);
     }
     return list;
   };
-  
+
   const testArr = [1, 2, [2.1, 2.2, 2.3], 3, [3.1], 4, [], 5, [6.1, 6.2, ['6.2.1']]];
   const list = generateList(testArr);
-  
-  document.body.append(list)
-  
+
+  document.body.append(list);
 })();
